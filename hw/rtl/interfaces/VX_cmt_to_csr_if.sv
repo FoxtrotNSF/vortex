2f6d6e06a58507a20004a76892f07485aa05ca0f
Noic Crouzet <crouzet.noic10@gmail.com>
2023-05-09 12:42:57 +0200

    Added signals in commit -> csr interface to be used with the runtime timing measurement

--- hw/rtl/interfaces/VX_cmt_to_csr_if.sv
+++ hw/rtl/interfaces/VX_cmt_to_csr_if.sv
@@ -6,6 +6,12 @@
 interface VX_cmt_to_csr_if ();
 
     wire                                valid;
+    wire [31:0]                         timeit_start_addr;
+    wire [31:0]                         timeit_end_addr;
+    reg                                 timeit_enable;
+    reg [`NW_BITS-1:0]                  timeit_active;
+    initial                             timeit_enable = 1'b0;
+    initial                             timeit_active = '0;
 `ifdef EXT_F_ENABLE
     wire [$clog2(6*`NUM_THREADS+1)-1:0] commit_size;
 `else
@@ -13,12 +19,20 @@ interface VX_cmt_to_csr_if ();
 `endif
     modport master (
         output valid,    
-        output commit_size
+        output commit_size,
+        output timeit_active,
+        input  timeit_start_addr,
+        input  timeit_end_addr,
+        input  timeit_enable
     );
 
     modport slave (
-        input valid,   
-        input commit_size
+        input  valid,
+        input  commit_size,
+        input  timeit_active,
+        output timeit_start_addr,
+        output timeit_end_addr,
+        output timeit_enable
     );
 
 endinterface
