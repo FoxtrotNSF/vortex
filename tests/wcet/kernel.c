d038c7e1c25511238085fe4fd6ded3d01f5643b8
Noic Crouzet <crouzet.noic10@gmail.com>
2023-05-09 12:48:04 +0200

    Added usage in test wcet kernel

--- tests/wcet/kernel.c
+++ tests/wcet/kernel.c
@@ -3,9 +3,10 @@
 #include <vx_spawn.h>
 #include <vx_print.h>
 #include "common.h"
+#include "timeit.h"
 
 #ifndef SIZE
-#define SIZE 16
+#define SIZE 4
 #endif
 
 void kernel_body(int task_id, kernel_arg_t* arg) {
@@ -48,13 +49,18 @@ void kernel_body(int task_id, kernel_arg_t* arg) {
     __asm__ __volatile__("nop");
     __asm__ __volatile__("nop");
     __asm__ __volatile__("nop");
-
+    __asm__ __volatile__("kernel_end: .global kernel_end":);
 }
 
 void main() {
 //    vx_printf("%d cores\n", vx_num_cores());
 //    vx_printf("%d warps\n", vx_num_warps());
 //    vx_printf("%d threads\n", vx_num_threads());
+    extern uint8_t kernel_body_last_intsr __asm__("kernel_end");
+    set_time_it(kernel_body, &kernel_body_last_intsr);
 	kernel_arg_t* arg = (kernel_arg_t*)KERNEL_ARG_DEV_MEM_ADDR;
 	vx_spawn_tasks(SIZE, (vx_spawn_tasks_cb)kernel_body, arg);
+    stop_timeit();
+    uint64_t cycles = read_time_it();
+    vx_printf("time : " "%llu\n", cycles);
 }
