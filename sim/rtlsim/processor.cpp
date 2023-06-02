3be2091a7e2c89d7ad79d71720110b8c9b343d73
Noic Crouzet <crouzet.noic10@gmail.com>
2023-06-02 11:25:23 +0200

    RTLSIM now emulates UART

--- sim/rtlsim/processor.cpp
+++ sim/rtlsim/processor.cpp
@@ -20,6 +20,7 @@
 #include <mem.h>
 
 #include <VX_config.h>
+#include <vx_uart.h>
 #include <ostream>
 #include <list>
 #include <queue>
@@ -285,7 +286,39 @@ private:
     device_->m_axi_rvalid  = 0;
     device_->m_axi_bvalid  = 0;
   }
-    
+
+  template<unsigned int N>
+  std::array<std::tuple<bool, uint32_t, uint32_t>,N> regs_access(uint32_t base_addr, uint32_t access_size, std::array<uint32_t,N> regs, uint8_t* data) {
+        uint32_t access_addr = base_addr % MEM_BLOCK_SIZE;
+        uint32_t access_reg_min = base_addr & (~0b11);
+        uint32_t access_reg_max = (base_addr + access_size);
+        std::array<std::tuple<bool, uint32_t, uint32_t>,N> out;
+        for(unsigned int req = 0; req < N; req++){
+            bool hit = (regs[req] >= access_reg_min && regs[req] < access_reg_max);
+            uint32_t size = std::max(access_size, 0x4U);
+            out[req] = std::make_tuple(hit, size, access_addr);
+        }
+        return out;
+  }
+
+  uint32_t get_reg_from(uint8_t* data, std::tuple<bool, uint32_t, uint32_t> extracted){
+      auto size          = std::get<1>(extracted);
+      auto ofs           = std::get<2>(extracted);
+      uint32_t val = (*(uint32_t*)(&data[ofs & (~0b11)]));
+      uint32_t bits = (size == 4) ? 0xFFFFFFFF : ((1<<(size*8))-1);
+      uint32_t mask = bits << (ofs & 0b11);
+      return  val & mask;
+  }
+
+  template<typename T>
+  void set_reg_to(uint8_t* data, std::tuple<bool, uint32_t, uint32_t> extracted, T reg){
+    auto size = std::get<1>(extracted);
+    auto ofs           = std::get<2>(extracted);
+    memset(data, 0, MEM_BLOCK_SIZE);
+    memcpy(data + ofs, ((uint8_t*) &reg) + (ofs & 0b11), size);
+  }
+  bool intr_enabled = false;
+
   void eval_axi_bus(bool clk) {
     if (!clk) {
       mem_rd_rsp_ready_ = device_->m_axi_rready;
@@ -365,6 +398,36 @@ private:
         uint64_t byteen = device_->m_axi_wstrb;
         unsigned base_addr = device_->m_axi_awaddr;
         uint8_t* data = (uint8_t*)(device_->m_axi_wdata.data());
+        uintptr_t access_size = pow(2, device_->m_axi_awsize);
+
+        auto regs_w = regs_access<4>(base_addr, access_size, {UART_RX_ADDR, UART_TX_ADDR, UART_CTRL_ADDR, UART_STAT_ADDR}, data);
+        if(std::get<0>(regs_w[0])) std::cout << "Unsupported write to UART RX register" << std::endl;
+        if(std::get<0>(regs_w[1])){
+            uint32_t reg_data = get_reg_from(data, regs_w[1]);
+//            std::cout << "UART TX : " << reg_data << " " << std::get<1>(regs_w[2]) << " " << std::get<2>(regs_w[2]) << std::endl;
+//            for(int c = 0; c < MEM_BLOCK_SIZE; c++)
+//                std::cout << std::hex <<  int(data[c]) << " ";
+//            std::cout << std::endl;
+            uart_fifo_t* uart_tx = (uart_fifo_t*) &reg_data;
+            std::cout << uart_tx->data;
+            std::cout.flush();
+        }
+        if(std::get<0>(regs_w[2])) {
+            uint32_t reg_data = get_reg_from(data, regs_w[2]);
+            uart_ctrl_t* uart_ctrl_data = (uart_ctrl_t*) &reg_data;
+            if(uart_ctrl_data->enable_intr)   intr_enabled=true;
+//            if(uart_ctrl_data->reset_tx_fifo) {
+//                std::cout << "UART TX flush" << std::endl;
+//                std::cout.flush();
+//            }
+//            if(uart_ctrl_data->reset_rx_fifo) {
+//                std::cout << "UART RX flush" << std::endl;
+//                std::cin.clear();
+//                std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+//            }
+//            if(uart_ctrl_data->enable_intr)   std::cout << "UART enable intr" << std::endl;
+        }
+        if(std::get<0>(regs_w[3])) std::cout << "Unsupported write to UART STAT register: " << std::hex << base_addr << " +: " << access_size << std::endl;
 
         // check console output
         if (base_addr >= IO_COUT_ADDR 
@@ -412,24 +475,58 @@ private:
       } else {
         // process reads
         auto mem_req = new mem_req_t();
-        mem_req->tag  = device_->m_axi_arid;   
-        mem_req->addr = device_->m_axi_araddr;
-        ram_->read(mem_req->block.data(), device_->m_axi_araddr, MEM_BLOCK_SIZE);
         mem_req->write = false;
         mem_req->ready = false;
-        pending_mem_reqs_.emplace_back(mem_req);
+        mem_req->tag  = device_->m_axi_arid;   
+        mem_req->addr = device_->m_axi_araddr;
 
-        // send dram request
-        ramulator::Request dram_req( 
-          device_->m_axi_araddr,
-          ramulator::Request::Type::READ,
-          std::bind([](ramulator::Request& dram_req, mem_req_t* mem_req) {
-              mem_req->ready = true;
-            }, placeholders::_1, mem_req),
-          0
-        );
-        dram_queue_.push(dram_req);
-      } 
+        uintptr_t access_size = pow(2, device_->m_axi_arsize);
+        bool bypass_ram_access = false;
+        auto regs_r = regs_access<4>(mem_req->addr, access_size, {UART_RX_ADDR, UART_TX_ADDR, UART_CTRL_ADDR, UART_STAT_ADDR}, nullptr);
+        if(std::get<0>(regs_r[0])){
+            //std::cout << "UART RX read" << std::endl;
+            uart_fifo_t uart_rx;
+            uart_rx.data = (std::cin.rdbuf()->in_avail() != -1) ? std::cin.rdbuf()->sbumpc() : 0 ;
+            set_reg_to(mem_req->block.data(), regs_r[0], uart_rx);
+            bypass_ram_access = true;
+        }
+        if(std::get<0>(regs_r[1])) std::cout << "Unsupported read to UART TX register" << std::endl;
+        if(std::get<0>(regs_r[2])){
+            //std::cout << "UART CTRL READ" << std::endl;
+            set_reg_to(mem_req->block.data(), regs_r[2], 0x00U);
+            bypass_ram_access = true;
+        }
+        if(std::get<0>(regs_r[3])){
+            //std::cout << "UART STAT READ" << std::endl;
+            uart_status_t uart_stat;
+            uart_stat.intr_enabled = intr_enabled;
+            uart_stat.rx_fifo_valid = (std::cin.rdbuf()->in_avail() != -1);
+            uart_stat.tx_fifo_full = 0;
+            uart_stat.tx_fifo_empty = 1;
+            //std::cout << uart_stat.tx_fifo_full << std::endl;
+            set_reg_to(mem_req->block.data(), regs_r[3], uart_stat);
+            bypass_ram_access = true;
+        }
+
+        if(bypass_ram_access){
+            mem_req->ready = true;
+        }else{
+            // data_read
+            ram_->read(mem_req->block.data(), device_->m_axi_araddr, MEM_BLOCK_SIZE);
+
+            // send dram request
+            ramulator::Request dram_req(
+              device_->m_axi_araddr,
+              ramulator::Request::Type::READ,
+              std::bind([](ramulator::Request& dram_req, mem_req_t* mem_req) {
+                  mem_req->ready = true;
+                }, placeholders::_1, mem_req),
+              0
+            );
+            dram_queue_.push(dram_req);
+        }
+        pending_mem_reqs_.emplace_back(mem_req);
+      }
     } 
 
     device_->m_axi_wready  = 1;
@@ -571,17 +668,17 @@ private:
 
     uint64_t get_clocks() {
 #ifdef AXI_BUS
-        return device_->Vortex_axi->vortex->vlSymsp->TOP__Vortex_axi__vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.__PVT__csr_unit__DOT__csr_data__DOT__csr_cycle;
+        return device_->Vortex_axi->vortex->vlSymsp->TOP__Vortex_axi__vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.csr_unit->csr_data->csr_cycle;
 #else
-        return device_->Vortex->vlSymsp->TOP__Vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.__PVT__csr_unit__DOT__csr_data__DOT__csr_cycle;
+        return device_->Vortex->vlSymsp->TOP__Vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.csr_unit->csr_data->csr_cycle;
 #endif
     }
 
     uint64_t get_instrs() {
 #ifdef AXI_BUS
-        return device_->Vortex_axi->vortex->vlSymsp->TOP__Vortex_axi__vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.__PVT__csr_unit__DOT__csr_data__DOT__csr_instret;
+        return device_->Vortex_axi->vortex->vlSymsp->TOP__Vortex_axi__vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.csr_unit->csr_data->csr_instret;
 #else
-        return device_->Vortex->vlSymsp->TOP__Vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.__PVT__csr_unit__DOT__csr_data__DOT__csr_instret;
+        return device_->Vortex->vlSymsp->TOP__Vortex__genblk2__BRA__0__KET____DOT__cluster__genblk2__BRA__0__KET____DOT__core__pipeline__execute.csr_unit->csr_data->csr_instret;
 #endif
     }
 
@@ -621,6 +718,7 @@ private:
     bool ready;  
     std::array<uint8_t, MEM_BLOCK_SIZE> block;
     uint64_t addr;
+    int size;
     uint64_t tag;
     bool write;
   } mem_req_t;
